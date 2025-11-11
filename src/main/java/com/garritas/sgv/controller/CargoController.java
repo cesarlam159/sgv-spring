64f6265c7cc84f5e4f940a5f670712fbe1a427c8
cesar <cesaraqp2@hotmail.com>
2025-11-10 21:49:45 -0500

    cambio prueba se hizo prueba de conexion

--- src/main/java/com/garritas/sgv/controller/CargoController.java
+++ src/main/java/com/garritas/sgv/controller/CargoController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
-@RequestMapping("/cargos")
+@RequestMapping("/cargos123")
 public class CargoController {
 
     private final CargoService cargoService;
